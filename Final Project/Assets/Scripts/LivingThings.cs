ceeada5c312c4db0eaed949c735d7a149a445b1f
Alessia <alessia@mipsscan.com>
2021-04-29 01:08:50 -0400

    First Push

---
+++ Final Project/Assets/Scripts/LivingThings.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LivingThings : MonoBehaviour
+{
+
+    public string cType;
+    public float health;
+    public float bAttack;
+
+    public LivingThings(string cType, float health, float bAttack)
+    {
+        this.cType = cType;
+        this.health = health;
+        this.bAttack = bAttack;
+    }
+
+    public virtual float attackDamage(float attack, float modifier)
+    {
+        return attack * modifier;
+    }
+
+}
