e03a61534a80de1350c3e68b6e7df7c141dff00c
Alessia <alessia@mipsscan.com>
2021-05-03 17:21:18 -0400

    Grid Generation

--- Final Project/Assets/Space Game/Scripts/GameManager.cs
+++ Final Project/Assets/Space Game/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
 using UnityEngine;
 
 [System.Serializable] 
@@ -10,9 +11,20 @@ public class GameManager : MonoBehaviour
     private const string FILEPATH_nebulaLayout = "NebulaLayout";
     private TextAsset DATA_nebulaLayout;
     public char[,] nebulaLayout;
+
+    [Header("Init Variables and Objects")] 
+    public Vector2 gridStartPos;
+    public int xOffset;
+    public int yOffset;
+    public GameObject ship;
+    public GameObject nebula;
     
     void Start()
     {
+        
+        //this is to first get the grid data - this should only happen ONCE, when you start the game. 
+        //we don't need to check if the file exists, because it is necessary for it to exist at the start
+        //It will never be first created at runtime
             DATA_nebulaLayout = Resources.Load(FILEPATH_nebulaLayout) as TextAsset;
             var tempLayout = DATA_nebulaLayout.text;
             // Debug.Log(tempLayout.ToCharArray().Length); - note, a char array keeps the newline characters
@@ -23,15 +35,39 @@ public class GameManager : MonoBehaviour
                 if (nebulaLayout == null)
                 {
                     nebulaLayout = new char[currentLine.Length, nebulaStrings.Length];
-                    Debug.Log("was null");
+                    //Debug.Log("was null");
                 }
-                for (int x = 0; x < currentLine.Length; x++)
+                for (int x = 0; x < currentLine.Length - 1; x++)
                 {
                     nebulaLayout[x, y] = currentLine[x];
+                    Debug.Log("X coord: " + x + "\nY coord: " + y + "\nValue: " + currentLine[x]);
                 }
             }
+            
+            MakeGrid();
+        
     }
 
+    void MakeGrid() //this is its own function if we ever need to re-draw the grid
+    {
+
+        for (int y = 0; y < nebulaLayout.GetLength(1); y++)
+        {
+            for (int x = 0; x < nebulaLayout.GetLength(0); x++)
+            {
+                if (nebulaLayout[x, y] == 'o')
+                {
+                    var playerObj = Instantiate<GameObject>(ship);
+                    playerObj.transform.position = new Vector2(x, y) + gridStartPos + new Vector2(x * xOffset, y * yOffset);
+                }
+
+                var newStar = Instantiate<GameObject>(nebula);
+                newStar.transform.position = new Vector2(x, y) + gridStartPos + new Vector2(x * xOffset, y * yOffset);
+
+            }
+        }
+    }
+    
     // Update is called once per frame
     void Update()
     {
